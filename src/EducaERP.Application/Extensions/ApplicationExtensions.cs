603c8593d110b0884f37c90ad8494e0ffe1b3339
mauridf <mauridf@gmail.com>
2025-05-05 18:01:47 -0300

    Incluindo dependicas

--- src/EducaERP.Application/Extensions/ApplicationExtensions.cs
+++ src/EducaERP.Application/Extensions/ApplicationExtensions.cs
@@ -1,19 +1,31 @@
 ﻿using EducaERP.Application.DTOs.Academics;
+using EducaERP.Application.DTOs.Enrollments;
+using EducaERP.Application.DTOs.Financial;
 using EducaERP.Application.DTOs.Institutions;
+using EducaERP.Application.DTOs.Library;
 using EducaERP.Application.DTOs.Students;
 using EducaERP.Application.DTOs.Teachers;
 using EducaERP.Application.Interfaces.Academics;
 using EducaERP.Application.Interfaces.Authentication;
+using EducaERP.Application.Interfaces.Enrollments;
+using EducaERP.Application.Interfaces.Financial;
 using EducaERP.Application.Interfaces.Institutions;
+using EducaERP.Application.Interfaces.Library;
 using EducaERP.Application.Interfaces.Students;
 using EducaERP.Application.Interfaces.Teachers;
 using EducaERP.Application.Services.Academics;
 using EducaERP.Application.Services.Authentication;
+using EducaERP.Application.Services.Enrollments;
+using EducaERP.Application.Services.Financial;
 using EducaERP.Application.Services.Institutions;
+using EducaERP.Application.Services.Library;
 using EducaERP.Application.Services.Students;
 using EducaERP.Application.Services.Teachers;
 using EducaERP.Application.Validators.Academics;
+using EducaERP.Application.Validators.Enrollments;
+using EducaERP.Application.Validators.Financial;
 using EducaERP.Application.Validators.Institutions;
+using EducaERP.Application.Validators.Library;
 using EducaERP.Application.Validators.Students;
 using EducaERP.Application.Validators.Teachers;
 using FluentValidation;
@@ -38,6 +50,12 @@ public static class ServiceCollectionExtensions
         services.AddScoped<ICourseDisciplineService, CourseService>();
         services.AddScoped<IDisciplineService, DisciplineService>();
         services.AddScoped<IGradeService, GradeService>();
+        services.AddScoped<IEnrollmentService, EnrollmentService>();
+        services.AddScoped<IInstallmentService, InstallmentService>();
+        services.AddScoped<ITuitionService, TuitionService>();
+        services.AddScoped<IBookService, BookService>();
+        services.AddScoped<IBookLoanService, BookLoanService>();
+        services.AddScoped<IBookReservationService, BookReservationService>();
 
         // AutoMapper
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
@@ -51,6 +69,12 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IValidator<CourseDTO>, CourseValidator>();
         services.AddScoped<IValidator<DisciplineDTO>, DisciplineValidator>();
         services.AddScoped<IValidator<GradeDTO>, GradeValidator>();
+        services.AddScoped<IValidator<EnrollmentDTO>, EnrollmentValidator>();
+        services.AddScoped<IValidator<InstallmentDTO>, InstallmentValidator>();
+        services.AddScoped<IValidator<TuitionDTO>, TuitionValidator>();
+        services.AddScoped<IValidator<BookDTO>, BookValidator>();
+        services.AddScoped<IValidator<BookLoanDTO>, BookLoanValidator>();
+        services.AddScoped<IValidator<BookReservationDTO>, BookReservationValidator>();
 
         return services;
     }
--- src/EducaERP.Infrastructure/Extensions/RepositoryExtensions.cs
+++ src/EducaERP.Infrastructure/Extensions/RepositoryExtensions.cs
@@ -1,13 +1,20 @@
-﻿using EducaERP.Application.Interfaces.Academics;
+﻿using EducaERP.Application.DTOs.Library.Responses;
+using EducaERP.Application.Interfaces.Academics;
 using EducaERP.Application.Interfaces.Authentication;
+using EducaERP.Application.Interfaces.Enrollments;
+using EducaERP.Application.Interfaces.Financial;
 using EducaERP.Application.Interfaces.Institutions;
+using EducaERP.Application.Interfaces.Library;
 using EducaERP.Application.Interfaces.Students;
 using EducaERP.Application.Interfaces.Teachers;
 using EducaERP.Core.Interfaces;
 using EducaERP.Infrastructure.Repositories;
 using EducaERP.Infrastructure.Repositories.Academics;
 using EducaERP.Infrastructure.Repositories.Authentication;
+using EducaERP.Infrastructure.Repositories.Enrollments;
+using EducaERP.Infrastructure.Repositories.Financial;
 using EducaERP.Infrastructure.Repositories.Institutions;
+using EducaERP.Infrastructure.Repositories.Library;
 using EducaERP.Infrastructure.Repositories.Students;
 using EducaERP.Infrastructure.Repositories.Teachers;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +36,12 @@ public static class RepositoryExtensions
         services.AddScoped<ICourseDisciplineRepository, CourseRepository>();
         services.AddScoped<IDisciplineRepository, DisciplineRepository>();
         services.AddScoped<IGradeRepository, GradeRepository>();
+        services.AddScoped<IEnrollmentRepository, EnrollmentRepository>();
+        services.AddScoped<IInstallmentRepository, InstallmentRepository>();
+        services.AddScoped<ITuitionRepository, TuitionRepository>();
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<IBookLoanRepository, BookLoanRepository>();
+        services.AddScoped<IBookReservationRepository, BookReservationRepository>();
 
         return services;
     }
